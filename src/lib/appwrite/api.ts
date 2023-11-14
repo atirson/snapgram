25b2fb7bba4f8ee4b518deb43a3d44fad83d5064
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2023-11-13 22:31:18 -0300

    chore: added saves, posts and users collection IDs

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,11 @@
-import { ID } from 'appwrite'
+import { ID, Query } from 'appwrite'
 import { INewUser } from '@/types'
-import { account } from '@/lib/appwrite/config'
+import {
+  account,
+  appwriteConfig,
+  avatars,
+  database,
+} from '@/lib/appwrite/config'
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -11,9 +16,74 @@ export async function createUserAccount(user: INewUser) {
       user.name,
     )
 
-    return newAccount
+    if (!newAccount) throw new Error()
+
+    const avatarUrl = avatars.getInitials(user.name)
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    })
+
+    return newUser
   } catch (error) {
     console.error(error)
     return error
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string
+  name: string
+  email: string
+  imageUrl: URL
+  username?: string
+}) {
+  try {
+    const newUser = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user,
+    )
+
+    if (!newUser) throw new Error()
+
+    return newUser
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function signInAccount(user: { email: string; password: string }) {
+  try {
+    const session = await account.createEmailSession(user.email, user.password)
+
+    return session
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get()
+
+    if (!currentAccount) throw new Error()
+
+    const currentUser = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal('accountId', currentAccount.$id)],
+    )
+
+    if (!currentUser) throw new Error()
+
+    return currentUser.documents[0]
+  } catch (error) {
+    console.error(error)
   }
 }
