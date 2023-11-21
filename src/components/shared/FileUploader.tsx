0e3f4b0d7220875e1982bcee73e0e1c658c96650
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2023-11-20 21:35:32 -0300

    feat: added component FileUploader

---
+++ src/components/shared/FileUploader.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { useDropzone, FileWithPath } from 'react-dropzone'
+
+type FileUploaderProps = {
+  fieldChange: (files: File[]) => void
+  mediaUrl: string
+}
+
+export const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+  const [files, setFiles] = useState<File[]>([])
+  const [fileUrl, setFileUrl] = useState('')
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFiles(acceptedFiles)
+      fieldChange(acceptedFiles)
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]))
+    },
+    [files],
+  )
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      'image/*': ['.png', '.gif', '.jpg', '.jpeg', '.svg'],
+    },
+  })
+
+  return (
+    <div
+      {...getRootProps()}
+      className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <>
+          <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
+            <img src={fileUrl} alt="Post Image" className="file_uploader-img" />
+          </div>
+          <p className="file_uploader-label">Click or drag photo to replace</p>
+        </>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            alt="File Uploader"
+            width={96}
+            height={77}
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag photo here
+          </h3>
+          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
+
+          <Button className="shad-button_dark_4">
+            Select from your local device
+          </Button>
+        </div>
+      )}
+    </div>
+  )
+}
