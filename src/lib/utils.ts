196c8cb4053baf6c1a4d8c04cbbed4fd2a510952
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2024-01-25 21:32:58 -0300

    feat: added method to format date to post card

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,31 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function timeAgo(dateString: string) {
+  // Convert the input date string to a JavaScript Date object
+  const date = new Date(dateString)
+
+  // Get the current date and time
+  const now = new Date()
+
+  // Calculate the time difference in milliseconds
+  const timeDiff = Number(now) - Number(date)
+
+  // Calculate the time difference in seconds, minutes, hours, and days
+  const seconds = Math.floor(timeDiff / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+
+  // Format the result based on the time difference
+  if (days > 1) {
+    return days + ' days ago'
+  } else if (hours > 1) {
+    return hours + ' hours ago'
+  } else if (minutes > 1) {
+    return minutes + ' minutes ago'
+  } else {
+    return seconds + ' seconds ago'
+  }
+}
